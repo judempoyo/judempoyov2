83880e887ac4ddf8d54ee874a992b34c90309c21
Jude mpoyo <mpoyojude0@gmail.com>
2025-06-10 00:22:49 +0200

    `Removed AppHeader and AppFooter components, added import statement for AppHeader in App.vue`

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-
+import AppHeader from '@/layouts/AppHeader.vue';
 </script>
 
 <template>
 
 
   <main>
+<AppHeader/>
 	 <h1 class="text-3xl mt-8 m-auto bg-red-500 font-bold underline">
     Hello world!
   </h1>
