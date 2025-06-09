421c343f4fb73cd7d14f72679e0cd4b6b098bdb9
Jude mpoyo <mpoyojude0@gmail.com>
2025-06-10 01:34:57 +0200

    add shadcn ui component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import Toogletheme from './toogletheme.vue';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import {
+    NavigationMenu,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { BookOpen, Folder, LayoutGrid, Menu, Search } from 'lucide-vue-next';
+import { computed } from 'vue';
+
+
+</script>
+
+<template>
+    <div>
+        <div class="border-b border-sideba	r-border/80">
+            <div class="mx-auto flex h-16 items-center px-4 md:max-w-7xl">
+                <!-- Mobile Menu -->
+                <div class="lg:hidden">
+                    <Sheet>
+                        <SheetTrigger :as-child="true">
+                            <Button variant="ghost" size="icon" class="mr-2 h-9 w-9">
+                                <Menu class="h-5 w-5" />
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent side="left" class="w-[300px] p-6">
+                            <SheetTitle class="sr-only">Navigation Menu</SheetTitle>
+                            <SheetHeader class="flex justify-start text-left">
+                                <AppLogoIcon class="size-6 fill-current text-black dark:text-white" />
+                            </SheetHeader>
+                            <div class="flex h-full flex-1 flex-col justify-between space-y-4 py-6">
+                                <nav class="-mx-3 space-y-1">
+                                    <Link
+                                        href="#"
+                                        class="flex items-center gap-x-3 rounded-lg px-3 py-2 text-sm font-medium hover:bg-accent"
+                                       
+                                    >
+                                       
+                                        {{ 'un' }}
+                                    </Link>
+                                </nav>
+                               
+                            </div>
+                        </SheetContent>
+                    </Sheet>
+                </div>
+
+               
+
+                <!-- Desktop Menu -->
+                <div class="hidden h-full lg:flex lg:flex-1">
+                    <NavigationMenu class="ml-10 flex h-full items-stretch">
+                        <NavigationMenuList class="flex h-full items-stretch space-x-2">
+                            <NavigationMenuItem  class="relative flex h-full items-center">
+                                <Link href="#">
+                                    <NavigationMenuLink
+                                       
+                                    >
+                                        
+                                        {{ 'un' }}
+                                    </NavigationMenuLink>
+                                </Link>
+                                <div
+                                    class="absolute bottom-0 left-0 h-0.5 w-full translate-y-px bg-black dark:bg-white"
+                                ></div>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenu>
+                </div>
+
+                <div class="ml-auto flex items-center space-x-2">
+                 
+										<Toogletheme />
+
+                </div>
+            </div>
+        </div>
+
+      
+    </div>
+</template>
