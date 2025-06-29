27be58629a7d990ade203b71dc89a366fccb43bd
Jude mpoyo <mpoyojude0@gmail.com>
2025-06-29 20:00:39 +0200

    `Refactor StyleSwitcher.vue: Extract script into setup block`

--- src/components/StyleSwitcher.vue
+++ src/components/StyleSwitcher.vue
@@ -1,5 +1,13 @@
+<script setup lang="ts">
+import { inject } from 'vue'
+
+
+	const currentStyle = inject('currentStyle')
+	const openStyleSelector = inject('openStyleSelector')
+
+</script>
 <template>
-  <button
+  <Button
     @click="openStyleSelector"
     class="fixed top-4 right-4 z-50 flex items-center gap-2 px-2 py-2 rounded-full transition-all"
     :class="{
@@ -10,21 +18,5 @@
     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
     </svg>
-  </button>
+  </Button>
 </template>
-
-<script setup lang="ts">
-import { inject } from 'vue'
-
-export default {
-  setup() {
-    const currentStyle = inject('currentStyle')
-    const openStyleSelector = inject('openStyleSelector')
-
-    return {
-      currentStyle,
-      openStyleSelector
-    }
-  }
-}
-</script>
